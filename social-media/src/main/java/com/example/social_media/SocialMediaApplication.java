5b4457a7b8d996233262f14068e3cce2e925949c
Kelsey Lin <kelseylin2000@gmail.com>
2024-09-24 11:31:45 +0800

    [feat] add annotation to allow Scheduling and TransactionManagement

--- social-media/src/main/java/com/example/social_media/SocialMediaApplication.java
+++ social-media/src/main/java/com/example/social_media/SocialMediaApplication.java
@@ -2,7 +2,11 @@ package com.example.social_media;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableTransactionManagement
+@EnableScheduling
 @SpringBootApplication
 public class SocialMediaApplication {
 
