12e62499aeb22c03ec0bd9d96e99eacf63ae969d
Kelsey Lin <kelseylin2000@gmail.com>
2024-10-02 14:05:53 +0800

    [feat] add time zone setting

--- social-media/src/main/java/com/example/social_media/SocialMediaApplication.java
+++ social-media/src/main/java/com/example/social_media/SocialMediaApplication.java
@@ -5,12 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import java.util.TimeZone;
+
 @EnableTransactionManagement
 @EnableScheduling
 @SpringBootApplication
 public class SocialMediaApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 		SpringApplication.run(SocialMediaApplication.class, args);
 	}
 
